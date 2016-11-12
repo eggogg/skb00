970ac5c2cb3e95c5d226f6a161e0b2d84f1ab2e0
Игорь Салтыков < >
2016-11-13 00:10:37 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+
+app.get('/', (req,res) => {
+	res.json({
+		hello: 'JS World',
+	});
+});
+
+app.get('/task2a', (req,res) => {
+	const sum = (+req.query.a || 0) + (+req.query.b || 0);
+//	res.json({
+	res.send( sum.toString());
+//	});
+});
+
+app.listen(3000, () => {
+	console.log('Example app listening on port 3000');
+})
+
